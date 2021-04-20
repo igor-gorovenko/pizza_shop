a575a289edf6874f3bf9408c9deea936c9bfc82d
Igor Gorovenko <igorforspeak@gmail.com>
2021-04-20 14:26:50 +0300

    add Category component
    add Category,
    upd export,

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Header from './Header'
+import React from 'react'
+
+import { Header, Categories } from './components'
 
 function App() {
 	return (
@@ -8,16 +10,7 @@ function App() {
 			<div className='content'>
 				<div className='container'>
 					<div className='content__top'>
-						<div className='categories'>
-							<ul>
-								<li className='active'>Все</li>
-								<li>Мясные</li>
-								<li>Вегетарианская</li>
-								<li>Гриль</li>
-								<li>Острые</li>
-								<li>Закрытые</li>
-							</ul>
-						</div>
+						<Categories items={['Мясные', 'Вегетерианская', 'Гриль', 'Острые', 'Закрытые']} />
 						<div className='sort'>
 							<div className='sort__label'>
 								<svg
